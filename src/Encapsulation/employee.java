140fee7011108ea44077b70e5041945c50129d27
swarna <kunatiswarna@gmail.com>
2023-09-26 19:16:53 +0530

    new class

--- src/Encapsulation/employee.java
+++ src/Encapsulation/employee.java
@@ -1,20 +1,59 @@
 package Encapsulation;
 
-public class employee {
- int rollnumber;
- String name;
- boolean isAttended;
+  public class employee {
+  private int rollnumber;
+  private String name;
+  private boolean isAttended;
+public Object setemp;
  
- public employee(int rollnumber) {
+ public int getRollnumber() {
+	return rollnumber;
+}
+
+
+public void setRollnumber(int rollnumber) {
+	System.out.println("210");
+	this.rollnumber = rollnumber;
+}
+
+
+public String getName() {
+	System.out.println("swarna");
+	return name;
+}
+
+
+public void setName(String name) {
+	System.out.println("latha");
+	this.name = name;
+}
+
+
+public employee(int rollnumber) {
+	System.out.println("205");
 	 this.rollnumber =rollnumber;
  }
 
+ 
  public void SetemployeeAttendence(boolean flag) {
 	 isAttended = flag;
 	 System.out.println("teamlead assigned attendence to employee");
  }
+ 
+ 
  public boolean getemployeeAttendence() {
 	 System.out.println("teamlead accessed employee attendence");
 	 return isAttended;
  }
+
+
+public void setemployeename(String flag) {
+	name =flag;
+	System.out.println("swarna");
+
+}
+public void getemployeerollnumber(int flag) {
+	rollnumber = flag;
+	System.out.println("210");
+}
 }
---
+++ src/Encapsulation/teamlead.java
@@ -0,0 +1,11 @@
+package Encapsulation;
+
+public class teamlead {
+ public static void main(String[] args) {
+	 employee s =new employee(101);
+	 s.SetemployeeAttendence(true);
+	 s.getemployeeAttendence();
+	 s.setemployeename(null);
+	 s.getemployeerollnumber(0);
+ }
+}
